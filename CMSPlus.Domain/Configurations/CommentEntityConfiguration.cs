c6f3a592282f9d1d7f17251576910fdfb27636d2
LapteacruCristian <75312122+LapteacruCristian@users.noreply.github.com>
2025-01-21 19:38:25 +0200

    Create comment entity and update topic entity for comment support
    Created CommentEntity and updated TopicEntity to support multiple comments per topic. Configured CommentEntity in ApplicationDbContext.

---
+++ CMSPlus.Domain/Configurations/CommentEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CMSPlus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CMSPlus.Domain.Configurations;
+
+public class CommentEntityConfiguration : IEntityTypeConfiguration<CommentEntity>
+{
+    public void Configure(EntityTypeBuilder<CommentEntity> builder)
+    {
+        builder.ToTable("Comments");
+        builder.Property(x => x.FullName).IsRequired();
+        builder.Property(x => x.Text).IsRequired();
+
+        builder.HasOne(c => c.Topic)
+            .WithMany(t => t.Comments)
+            .HasForeignKey(c => c.TopicId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
